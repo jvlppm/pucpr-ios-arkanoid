c8d8004656e43e72d7befd2d698a868ef28664ab
Joao Vitor <joao@DVTI-NB-ROBERTO.local>
2014-10-26 22:09:26 -0200

    Initial message

--- pucpr-ios-arkanoid/GameScene.swift
+++ pucpr-ios-arkanoid/GameScene.swift
@@ -9,18 +9,29 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let startMessage: SKLabelNode
     let bar: SKSpriteNode
+    
+    var balls: Array<SKSpriteNode>
     var moveTouch: UITouch?
+    
+    var waitingToBegin: Bool
+    
     
     required init?(coder aDecoder: NSCoder) {
         self.bar = SKSpriteNode(color: UIColor.greenColor(), size: CGSize(width: 64, height: 8))
         self.bar.anchorPoint = CGPoint(x: 0.5, y: 0)
+        self.startMessage = SKLabelNode(text: "Mova a barra para iniciar")
+        self.balls = []
+        waitingToBegin = true
         super.init(coder: aDecoder)
     }
     
     override func didMoveToView(view: SKView) {
         bar.position = CGPoint(x: self.frame.width / 2, y: bar.size.height)
         self.addChild(bar)
+        startMessage.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        restartBall()
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -32,6 +43,10 @@ class GameScene: SKScene {
 
             if grab(location, location: self.bar.position) {
                 self.moveTouch = touch
+                if waitingToBegin {
+                    self.removeChildrenInArray([self.startMessage])
+                    waitingToBegin = false
+                }
                 break
             }
         }
@@ -64,6 +79,17 @@ class GameScene: SKScene {
         dist.normalize()
 
         return dist.length() <= 50
+    }
+    
+    func restartBall() {
+        self.removeChildrenInArray(self.balls)
+        
+        let ball = SKSpriteNode(color: UIColor.blueColor(), size: CGSize(width: 8, height: 8))
+        ball.position = CGPoint(x: self.frame.width / 2, y: self.frame.height * 0.3)
+        self.addChild(ball)
+        self.balls.append(ball)
+        waitingToBegin = true
+        self.addChild(startMessage)
     }
    
     override func update(currentTime: CFTimeInterval) {
